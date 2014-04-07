24f05ae18b97204e2057014a9613ab61fe1684a4
Andrew.Stewart <Andrew.Stewart@advancedcomputersoftware.com>
2014-04-07 13:18:02 +0100

    Refactored angular controller

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,41 +3,28 @@ var teamCityMonitor = angular.module('teamCityMonitor', [])
         $locationProvider.html5Mode(true);
     });
 
+function buildController($scope, $http, $timeout, $location) {
 
-function parseDefectString(defects) {
-    var defectString = '';
-
-    for(var i =0; i < defects.length; i++) {
-        defectString = defectString + defects[i] + ", ";
+    function updateScreen(data) {
+        try {
+            $scope.buildInformation = data;
+        } finally {
+            $scope.refreshInProgress = false;
+            waitThenUpdateScreen();
+        }
     }
 
-    return defectString.substring(0, defectString.length - 2);
-}
+    function waitThenUpdateScreen() {
+        $timeout(refresh, 10000);
+    }
 
-var refreshBuildData = function(data) {
-    for (var i = data.length - 1; i >= 0; i--) {
-        var buildResults = data[i];
-        //buildResults.defects = parseDefectString(buildResults.defects);
-    };
-   
-    return data;
-}
-
-function buildController($scope, $http, $timeout, $location) {
-    var refresh = function () {
+    function refresh() {
         $scope.refreshInProgress = true;
-        $http.get('/buildstatus' + $location.path()).success(function (data) {
-            try {
-                $scope.buildInformation = refreshBuildData(data);
-            } finally {
-                $scope.refreshInProgress = false;
-                $timeout(refresh, 10000);
-            }
-        })
-        .error(function () {
-            $timeout(refresh, 10000);
-        });
+        $http.get('/buildstatus' + $location.path())
+                .success(updateScreen)
+                .error(waitThenUpdateScreen);
     };
+
     refresh();
 }
 
