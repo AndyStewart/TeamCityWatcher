e173734c10ae65888625b20c0116eeb6b140f772
Andrew Stewart <andrew.stewart@i-nnovate.net>
2015-07-07 22:35:15 +0100

    Refactored build rendering

--- public/javascripts/build/buildmonitor.js
+++ public/javascripts/build/buildmonitor.js
@@ -1,46 +1,47 @@
-function getProject(projectName, callback) {
+function get(url, callback) {
 	var xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function() {
 	    if (xhr.readyState == 4) {
 	        callback(null, JSON.parse(xhr.responseText));
 	    }
 	}
-	xhr.open('GET', '/buildstatus/' + projectName, true);
+	xhr.open('GET', url, true);
 	xhr.send(null);
 }
 
-function loadMonitor(callback) {
-	var projectName = "E-Subs";
-	getProject(projectName, function(err, result) {
+function loadProject(projectName, callback) {
+	get('/buildstatus/' + projectName, function(err, result) {
 		callback(err, { name: projectName, pipelines: result });
 	});
 }
 
-function renderBuild(project) {
-    var pipelines = [];
-    for (var i = 0; i < project.pipelines.length; i++) {
-        var pipeline = project.pipelines[i];
-        pipelines.push(React.createElement("div", {className: "row Pipeline"}, 
-                      React.createElement("div", {className: "col-md-12"}, 
-                        React.createElement("h4", null, "#", pipeline.number, " started at ", pipeline.startTime), 
-                        React.createElement(Changes, {pipeline: pipeline}), 
-						React.createElement("div", {className: "row"}, 
-                        renderPipeline(pipeline)
-						)
-                      )
-                    ));
-    };
+function renderPipeline(pipeline) {
+	var builds = pipeline.builds.map(renderBuild);
+	return React.createElement("div", {className: "row Pipeline"}, 
+				React.createElement("div", {className: "col-md-12"}, 
+					React.createElement("h4", null, "#", pipeline.number, " started at ", pipeline.startTime), 
+					React.createElement(Changes, {pipeline: pipeline}), 
+					React.createElement("div", {className: "row"}, 
+					builds
+					)
+				)
+			);
+}
 
-	var component = React.createElement("div", null, React.createElement("div", {className: "row"}, 
-      					React.createElement("div", {className: "col-md-12"}, 
-        					React.createElement("h1", {className: "text-center"}, project.name)
-        				)
-        			), 
-                    pipelines);
+function renderProject(project) {
+    var pipelines = project.pipelines.map(renderPipeline);
+	var component = React.createElement("div", null, 
+						React.createElement("div", {className: "row"}, 
+      						React.createElement("div", {className: "col-md-12"}, 
+        						React.createElement("h1", {className: "text-center"}, project.name)
+        					)
+        				), 
+                    	pipelines
+					);
 
 	React.render(component, document.getElementById('BuildMonitor'))
 }
 
-loadMonitor(function(err, result) {
-	renderBuild(result);
+loadProject("E-Subs", function(err, project) {
+	renderProject(project);
 });
--- public/javascripts/components/buildmonitor.js
+++ public/javascripts/components/buildmonitor.js
@@ -1,46 +1,47 @@
-function getProject(projectName, callback) {
+function get(url, callback) {
 	var xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function() {
 	    if (xhr.readyState == 4) {
 	        callback(null, JSON.parse(xhr.responseText));
 	    }
 	}
-	xhr.open('GET', '/buildstatus/' + projectName, true);
+	xhr.open('GET', url, true);
 	xhr.send(null);
 }
 
-function loadMonitor(callback) {
-	var projectName = "E-Subs";
-	getProject(projectName, function(err, result) {
+function loadProject(projectName, callback) {
+	get('/buildstatus/' + projectName, function(err, result) {
 		callback(err, { name: projectName, pipelines: result });
 	});
 }
 
-function renderBuild(project) {
-    var pipelines = [];
-    for (var i = 0; i < project.pipelines.length; i++) {
-        var pipeline = project.pipelines[i];
-        pipelines.push(<div className="row Pipeline">
-                      <div className="col-md-12">
-                        <h4>#{pipeline.number} started at {pipeline.startTime}</h4>
-                        <Changes pipeline={pipeline} />
-						<div className="row">
-                        {renderPipeline(pipeline)}
-						</div>
-                      </div>
-                    </div>);
-    };
+function renderPipeline(pipeline) {
+	var builds = pipeline.builds.map(renderBuild);
+	return <div className="row Pipeline">
+				<div className="col-md-12">
+					<h4>#{pipeline.number} started at {pipeline.startTime}</h4>
+					<Changes pipeline={pipeline} />
+					<div className="row">
+					{builds}
+					</div>
+				</div>
+			</div>;
+}
 
-	var component = <div><div className="row">
-      					<div className="col-md-12">
-        					<h1 className="text-center">{project.name}</h1>
+function renderProject(project) {
+    var pipelines = project.pipelines.map(renderPipeline);
+	var component = <div>
+						<div className="row">
+      						<div className="col-md-12">
+        						<h1 className="text-center">{project.name}</h1>
+        					</div>
         				</div>
-        			</div>
-                    {pipelines}</div>;
+                    	{pipelines}
+					</div>;
 
 	React.render(component, document.getElementById('BuildMonitor'))
 }
 
-loadMonitor(function(err, result) {
-	renderBuild(result);
+loadProject("E-Subs", function(err, project) {
+	renderProject(project);
 });
