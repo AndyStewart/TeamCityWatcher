13391705659bd63cbb001ec871bf733468e51812
Andrew Stewart <andrew.stewart@i-nnovate.net>
2015-10-02 22:49:46 +0100

    Slight refactor

--- specs/app/viewingBuilds.js
+++ specs/app/viewingBuilds.js
@@ -10,34 +10,35 @@ describe('Builds', function() {
     // });
 
     it('build server results are converted to builds', function (done) {
+    	function findBuildsOnServer() {
+    		return new Promise(function (resolve, reject) {
+	    		resolve({
+	    			"build": [
+				        {
+				            "id": 40930,
+				            "buildTypeId": "IrisLawBusiness_UATEnvironment",
+				            "number": "11510",
+				            "status": "FAILURE",
+				            "state": "finished",
+				            "href": "/guestAuth/app/rest/builds/id:40930",
+				            "webUrl": "http://solicitors-build:8023/viewLog.html?buildId=40930&buildTypeId=IrisLawBusiness_UATEnvironment"
+				        },
+				        {
+				            "id": 40927,
+				            "buildTypeId": "bt12",
+				            "number": "11510",
+				            "status": "SUCCESS",
+				            "state": "finished",
+				            "branchName": "refs/heads/develop",
+				            "defaultBranch": true,
+				            "href": "/guestAuth/app/rest/builds/id:40927",
+				            "webUrl": "http://solicitors-build:8023/viewLog.html?buildId=40927&buildTypeId=bt12"
+				        }]
+	    		});
+	    	});
+    	}
 
-    	var serverResults = new Promise(function (resolve, reject) {
-    		resolve({
-    			"build": [
-			        {
-			            "id": 40930,
-			            "buildTypeId": "IrisLawBusiness_UATEnvironment",
-			            "number": "11510",
-			            "status": "FAILURE",
-			            "state": "finished",
-			            "href": "/guestAuth/app/rest/builds/id:40930",
-			            "webUrl": "http://solicitors-build:8023/viewLog.html?buildId=40930&buildTypeId=IrisLawBusiness_UATEnvironment"
-			        },
-			        {
-			            "id": 40927,
-			            "buildTypeId": "bt12",
-			            "number": "11510",
-			            "status": "SUCCESS",
-			            "state": "finished",
-			            "branchName": "refs/heads/develop",
-			            "defaultBranch": true,
-			            "href": "/guestAuth/app/rest/builds/id:40927",
-			            "webUrl": "http://solicitors-build:8023/viewLog.html?buildId=40927&buildTypeId=bt12"
-			        }]
-    		});
-    	});
-
-        buildQueries.findAll(function() { return serverResults }).then(function(result) {
+        buildQueries.findAll(findBuildsOnServer).then(function(result) {
         	result.length.should.equal(2);
         	result[0].id.should.equal(40930);
         	result[0].status.should.equal("FAILURE");
