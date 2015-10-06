9c46e4d81c6e953ee94aabda2fc0d64885033ff3
Andrew Stewart <andrew.stewart@i-nnovate.net>
2015-10-06 22:18:54 +0100

    Refactored test

--- specs/app/buildSpecs.js
+++ specs/app/buildSpecs.js
@@ -1,44 +1,53 @@
+require("babel/register");
+
 var should = require("should");
 var Promise = require("bluebird");
 var builds = require("../../app/queries/builds");
 
 describe('Builds', function() {
-  	describe('Viewing Builds', function () {
-	    it('build server results are converted to builds', function (done) {
-	    	function buildResult(build) {
-	    		return {
-				            "id": build.id,
-				            "buildTypeId": "bt12",
-				            "number": "11510",
-				            "status": build.status,
-				            "state": "finished",
-				            "href": "/guestAuth/app/rest/builds/id:40930",
-				            "webUrl": "http://server-name/viewLog.html?buildId=40930&buildTypeId=bt12"
-				        };
-	    	}
+	function buildResult(build) {
+		return {
+		            "id": build.id,
+		            "buildTypeId": "bt12",
+		            "number": "11510",
+		            "status": build.status,
+		            "state": "finished",
+		            "href": "/guestAuth/app/rest/builds/id:40930",
+		            "webUrl": "http://server-name/viewLog.html?buildId=40930&buildTypeId=bt12"
+		        };
+	}
 
-	    	function allBuilds() {
-				return new Promise(function (resolve, reject) {
-							    		resolve({
-							    			"build": [
-										        buildResult({id: 40930, status: "FAILURE"}),
-										        buildResult({id: 40927, status: "SUCCESS"})]
-							    		});
-							    	});
-			}
+  	describe('Get latest builds from server', function () {
+		function allBuilds() {
+			return new Promise(function (resolve, reject) {
+						    		resolve({
+						    			"build": [
+									        buildResult({id: 40930, status: "FAILURE"}),
+									        buildResult({id: 40927, status: "SUCCESS"})]
+						    		});
+						    	});
+		}
 
-	    	function verify(result) {
-				result.length.should.equal(2);
-				result[0].id.should.equal(40930);
-				result[0].status.should.equal("FAILURE");
+		it('returns correct amount of results', function (done) {
+    		builds.all(allBuilds, function(r) { r.length.should.equal(2) });
+    		done();
+    	});
 
-				result[1].id.should.equal(40927);
-				result[1].status.should.equal("SUCCESS");
-				result.length.should.equal(2);
-				done();
-	        }
+		it('returns the id of the build', function (done) {
+    		builds.all(allBuilds, function(r){ 
+    			r[0].id.should.equal(40930);
+    			r[1].id.should.equal(40927);
+    			done();
+    		});
+    	});
 
-	    	builds.all(allBuilds, verify);
-		});
+		it('returns the status of the build', function (done) {
+    		builds.all(allBuilds, function(r){ 
+    			r[0].status.should.equal("FAILURE");
+    			r[1].status.should.equal("SUCCESS");
+    			done();
+    		});
+    	});
+
 	});
 });
