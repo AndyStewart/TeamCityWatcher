934562f0a9580f07f049667765666aa4452f87cf
Andrew Stewart <Andrew.Stewart@advancedcomputersoftware.com>
2015-10-07 16:26:48 +0100

    Change scoping of functions

--- app/queries/builds.js
+++ app/queries/builds.js
@@ -1,21 +1,22 @@
-
-function createBuild(serverBuild) {
-	return { status: serverBuild.status, id: serverBuild.id};
-}
-
-function convertToBuilds(serverResult) {
-	return serverResult.build.map(createBuild);
-}
-
 function all(serverResults) {
+
+	function createBuild(serverBuild) {
+		return { status: serverBuild.status, id: serverBuild.id};
+	}
+
+	function convertToBuilds(serverResult) {
+		return serverResult.build.map(createBuild);
+	}
+
 	return serverResults().then(convertToBuilds);
 }
 
-function convertToBuildInformation(serverBuild) {
-	return {id : serverBuild.id, status: serverBuild.status, name: serverBuild.buildType.name, statusText: serverBuild.statusText};
-}
-
 function information(id, buildInformation) {
+
+	function convertToBuildInformation(serverBuild) {
+		return {id : serverBuild.id, status: serverBuild.status, name: serverBuild.buildType.name, statusText: serverBuild.statusText};
+	}
+
 	return buildInformation(id).then(convertToBuildInformation);
 }
 
